3ff85a6c5f395a8a03ac8220d595144fe8aed4ab
mhero <gdmarav374@gmail.com>
2020-01-28 09:16:44 -0500

    Create game basic flow
    - User enters their name, game name, and task name

--- poker-client/src/app/App.js
+++ poker-client/src/app/App.js
@@ -1,22 +1,97 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+const io = require("socket.io-client");
+const HOST = "http://localhost:8000";
+const socket = io.connect(HOST);
+const axios = require('axios').default;
 
 function App() {
+
+  const [step, setStep] = useState(1);
+
+  const [userId, setUserId] = useState(null);
+  const [gameId, setGameId] = useState(null);
+  const [taskId, setTaskId] = useState(null);
+
+  const [gameName, setGameName] = useState(null);
+  const [userName, setUserName] = useState(null);
+  const [taskName, setTaskName] = useState(null);
+
+  const post = (url, object, callback) => {
+    axios.post(
+      url, object
+    )
+    .then((response) => {
+      callback(response.data.id);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  const createUser = () => {
+    post(`${HOST}/api/users`, { name: userName }, setUserId);
+  }
+
+  const createTask = (game) => {
+    post(`${HOST}/api/tasks`, { name: taskName, game_id: game }, setTaskId);
+  }
+
+  const createGame = () => {
+    createUser();
+    axios.post(
+      `${HOST}/api/games`, { name: gameName }
+    )
+    .then((response) => {
+      setGameId(response.data.id);
+      createTask(response.data.id);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+    setStep(2);
+  }
+
+  const enterGame = () => {
+    createUser();
+    setStep(2);
+  }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { step === 1 && 
+        <div>
+          <div>
+            <input type="text" id="game" name="game" 
+                placeholder="Enter game name"
+                onChange={(event) => setGameName(event.target.value) }/>
+            <input type="text" id="user" name="user" 
+                placeholder="Enter your name"
+                onChange={(event) => setUserName(event.target.value) }/>
+            <input type="text" id="user" name="user" 
+                placeholder="Enter your first task"
+                onChange={(event) => setTaskName(event.target.value) }/>
+            <button type="button" onClick={createGame}>Create game!</button>
+          </div>
+          or
+          <div>
+            <input type="text" id="game" name="game" 
+                  placeholder="Enter game id"
+                  onChange={(event) => setGameId(event.target.value) }/>
+            <input type="text" id="user" name="user" 
+                placeholder="Enter your name"
+                onChange={(event) => setUserName(event.target.value) }/>
+            <button type="button" onClick={enterGame}>Join game!</button>
+          </div>
+        </div>
+      }
+      { step === 2 && 
+        <div>
+          <div>User ID: {userId}</div>
+          <div>Game ID: {gameId}</div>
+          <div>Task ID: {taskId}</div>
+        </div>
+      }
     </div>
   );
 }

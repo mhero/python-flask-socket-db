ae785328ce0f487677ceed7040557c1fb3cba159
mhero <gdmarav374@gmail.com>
2020-01-29 09:51:33 -0500

    Get latest active task from game

--- services/task.py
+++ services/task.py
@@ -1,4 +1,4 @@
-from models import Task, TaskSchema, db, Status
+from models import Task, TaskSchema, db, Status, Game
 from sqlalchemy.exc import SQLAlchemyError
 
 
@@ -13,4 +13,14 @@ class TaskService:
             return schema.dump(task)
         except SQLAlchemyError as e:
             error = str(e.__dict__)
+            return error
+
+    def get_active(game_id):
+        try:
+            return db.session.query(Task.id, Task.name).\
+                        join(Game, Task.id == Game.id).\
+                        filter(Task.status == Status.active).\
+                        filter(Game.id == game_id).first()[0]
+        except SQLAlchemyError as e:
+            error = str(e.__dict__['orig'])
             return error
